d5533eb3bb59ff58806411901e3e053d8d70f4b8
08darknight1 <39628779+08darknight1@users.noreply.github.com>
2020-04-03 23:28:43 -0300

    Primeiro up do projeto: movimentação personagem e camera

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    private GameObject playerGO;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerGO = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var resultPos = playerGO.transform.position.x - gameObject.transform.position.x;
+         
+        if (resultPos >= 0.8 || resultPos <= -0.8)
+        {
+            var playerPos = new Vector3(playerGO.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, playerPos, 0.01f);
+        }
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public int Speed;
+
+    public int maxSpeed;
+
+    private bool isMoving = false;
+    
+    private Rigidbody playerRGB;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRGB = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerRGB.velocity.magnitude > maxSpeed){
+            playerRGB.velocity = Vector3.ClampMagnitude(playerRGB.velocity, maxSpeed);
+        }
+        
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {   
+            isMoving = true;
+            var hAxis = Input.GetAxis("Horizontal");
+            var forceValue = hAxis * Speed;
+            playerRGB.AddForce(new Vector3(forceValue, gameObject.transform.position.y,gameObject.transform.position.z));
+        }
+        else
+        {
+            isMoving = false;
+        }
+        
+    }
+}
