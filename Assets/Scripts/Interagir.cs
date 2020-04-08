341d464d49721ce82e1b207beacbe5b212110189
NinguemBravo <58083466+NinguemBravo@users.noreply.github.com>
2020-04-08 15:58:42 -0300

    Adicionado mecânica primal de interação

---
+++ Assets/Scripts/Interagir.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interagir : MonoBehaviour
+{
+    public GameObject Area;
+    public GameObject Luz;
+
+    private void Start()
+    {
+        Luz.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetButton("E"))
+            {
+                Luz.SetActive(true);
+            }
+        }
+    }
+}
