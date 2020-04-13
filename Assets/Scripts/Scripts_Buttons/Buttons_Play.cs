772b53d61fa7ee0da070b55696b412289f23b95e
NinguemBravo <58083466+NinguemBravo@users.noreply.github.com>
2020-04-13 17:31:11 -0300

    Menu Inicial

---
+++ Assets/Scripts/Scripts_Buttons/Buttons_Play.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons_Play : MonoBehaviour
+{
+    public void Button_Play()
+    {
+        Invoke("MudarScene", 2f);
+    }
+    private void MudarScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Jogo");
+    }
+    public void Button_Sair()
+    {
+        Invoke("Sair",2f);
+    }
+    private void Sair()
+    {
+        Application.Quit();
+        Debug.Log("Saiu");
+    }
+}
