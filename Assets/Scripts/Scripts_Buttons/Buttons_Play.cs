6cf5a11e94b6fbb02a261ea0637a7b249051b1b9
NinguemBravo <58083466+NinguemBravo@users.noreply.github.com>
2020-05-21 16:48:39 -0300

    oi

--- Assets/Scripts/Scripts_Buttons/Buttons_Play.cs
+++ Assets/Scripts/Scripts_Buttons/Buttons_Play.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Buttons_Play : MonoBehaviour
 {
+    private AudioSource audio;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     public void Button_Play()
     {
+        audio.Play();
         Invoke("MudarScene", 2f);
     }
     private void MudarScene()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Jogo");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("CharCreatorTest");
     }
     public void Button_Sair()
     {
+        audio.Play();
         Invoke("Sair",2f);
     }
     private void Sair()
